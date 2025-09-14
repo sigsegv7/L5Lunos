bb5cc59c93b07c87177eafdce9afe43be461b00b
Ian Moffett <ian@osmora.org>
2025-09-13 23:41:09 -0400

    os: init: Panic at end of kernel
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- src/sys/os/os_init.c
+++ src/sys/os/os_init.c
@@ -1,4 +1,5 @@
 #include <sys/cdefs.h>
+#include <sys/panic.h>
 #include <sys/cpuvar.h>
 
 struct pcore g_bsp;
@@ -10,5 +11,6 @@ __dead void
 main(void)
 {
     cpu_conf(&g_bsp);
+    panic("end of kernel reached\n");
     for (;;);
 }
