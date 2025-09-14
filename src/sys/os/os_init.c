3b9e8a1aa73fed52268def39400b063030d0bc5c
Ian Moffett <ian@osmora.org>
2025-09-14 15:25:13 -0400

    os: Add early boot message
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- src/sys/os/os_init.c
+++ src/sys/os/os_init.c
@@ -1,5 +1,6 @@
 #include <sys/cdefs.h>
 #include <sys/panic.h>
+#include <sys/syslog.h>
 #include <sys/cpuvar.h>
 
 struct pcore g_bsp;
@@ -11,6 +12,7 @@ __dead void
 main(void)
 {
     cpu_conf(&g_bsp);
+    printf("booting l5 lunos v0.0.1...\n");
     panic("end of kernel reached\n");
     for (;;);
 }
