48e296d044d972fbd3d5a8a386cc874dd7334d86
Ian Moffett <ian@osmora.org>
2025-09-13 23:24:49 -0400

    build: Point includes to string dir as well
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- src/sys/conf/sys.mk
+++ src/sys/conf/sys.mk
@@ -8,7 +8,7 @@ override MD_CFLAGS = -mno-80387 -mno-mmx -mno-3dnow \
 override MI_CFLAGS = -fexceptions --std=gnu11 -ffreestanding -fno-stack-protector -fno-pic \
 	-Werror=implicit -Werror=implicit-function-declaration \
 	-Werror=implicit-int -Werror=int-conversion \
-    -Werror=missing-prototypes                   \
+    -Werror=missing-prototypes  -Isys/include/lib/ \
 	-Werror=incompatible-pointer-types -Werror=int-to-pointer-cast \
 	-Werror=return-type -mno-red-zone -mcmodel=kernel \
 	-D_KERNEL -Wno-pointer-sign -MMD -nostdinc \
