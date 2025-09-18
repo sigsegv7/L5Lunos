df5eb26ab729a3230b0fc087b734a29e92697356
Ian Moffett <ian@osmora.org>
2025-09-18 13:54:08 -0400

    kern: Bump L5 version to v0.0.2
    Signed-off-by: Ian Moffett <ian@osmora.org>

--- src/sys/include/sys/sysvar.h
+++ src/sys/include/sys/sysvar.h
@@ -27,64 +27,10 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <sys/cdefs.h>
-#include <sys/panic.h>
-#include <sys/sysvar.h>
-#include <sys/syslog.h>
-#include <sys/proc.h>
-#include <sys/cpuvar.h>
-#include <os/sched.h>
-#include <os/elfload.h>
-#include <acpi/acpi.h>
-#include <io/cons/cons.h>
-#include <vm/vm.h>
-#include <logo.h>
+#ifndef _SYS_SYSVAR_H_
+#define _SYS_SYSVAR_H_
 
-struct pcore g_bsp;
-struct proc g_rootproc;
+/* Kernel version */
+#define _L5_VERSION "v0.0.2"
 
-static void
-boot_print(void)
-{
-    printf("%s\n", g_LOGO);
-    printf("Copyright (c) 2025 Ian Marco Moffett, et al\n");
-    printf("booting l5 lunos %s...\n", _L5_VERSION);
-}
-
-/*
- * Kernel entrypoint
- */
-__dead void
-main(void)
-{
-    struct loaded_elf elf;
-    struct pcore *core;
-    int error;
-
-    acpi_early_init();
-
-    cons_init();
-    syslog_toggle(true);
-    boot_print();
-
-    cpu_conf(&g_bsp);
-    vm_init();
-
-    cpu_init(&g_bsp);
-    bsp_ap_startup();
-
-    sched_init();
-    core = this_core();
-    proc_init(&g_rootproc, 0);
-    core->curproc = &g_rootproc;
-
-    error = elf_load("/usr/bin/init", &g_rootproc, &elf);
-    if (error < 0) {
-        panic("could not load init\n");
-    }
-
-    md_set_ip(&g_rootproc, elf.entrypoint);
-    md_proc_kick(&g_rootproc);
-    panic("end of kernel reached\n");
-    for (;;);
-}
+#endif  /* !_SYS_SYSVAR_H_ */
