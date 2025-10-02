5314ae3b0e64fccc12c684c4e74c6d039adf09f9
Ian Moffett <ian@osmora.org>
2025-10-01 21:36:06 -0400

    kern: np: Add initial IR engine (PIIR)
    This commit introduces the initial sources for the intermediate
    representation (IR) engine [called PIIR (PI-IR)] to PIRHO
    
    PIIR: [NOUN]: border, boundary, frontier
    
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ src/sys/include/np/piir.h
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2025 Ian Marco Moffett and L5 engineers
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _NP_PIIR_H_
+#define _NP_PIIR_H_
+
+#include <sys/types.h>
+#include <sys/cdefs.h>
+#include <os/spinlock.h>
+#include <os/np.h>
+
+typedef uint8_t ir_byte_t;
+
+/*
+ * The maxiumum size of the bytecode stack per
+ * translation unit
+ */
+#define PIIR_STACK_SIZE 4096
+
+/* PIIR opcodes */
+#define PIIR_NOP      0x00
+#define PIIR_LOAD_R8  0x01
+#define PIIR_LOAD_R16 0x02
+#define PIIR_LOAD_R32 0x03
+#define PIIR_LOAD_R64 0x04
+
+/*
+ * Represents the Pi-IR (PIIR) bytecode stack
+ *
+ * @opstore: The actual stack
+ * @op_i: Index into the stack
+ */
+struct piir_stack {
+    ir_byte_t opstore[PIIR_STACK_SIZE];
+    uint16_t op_i;
+    struct spinlock lock;
+};
+
+/*
+ * Allocate a new PIIR stack
+ *
+ * @work: Current work
+ * @resp: Result pointer is written here
+ *
+ * Returns zero on success, otherwise a less than zero
+ * value on failure.
+ */
+int piir_stack_new(struct np_work *work, struct piir_stack **resp);
+
+/*
+ * Push a byte to the IR stack
+ *
+ * @stack: Stack to push to
+ * @byte: Byte to push
+ *
+ * Returns zero on success, otherwise a less than zero
+ * value on failure.
+ */
+int piir_push(struct piir_stack *stack, ir_byte_t byte);
+
+/*
+ * Pop a byte from the IR stack
+ *
+ * @stack: Stack to pop from
+ *
+ * Returns the byte on success, otherwise a less than
+ * zero value if the stack is empty
+ */
+int piir_pop(struct piir_stack *stack);
+
+#endif  /* !_NP_PIIR_H_ */
---
+++ src/sys/np/core/np_piir.c
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2025 Ian Marco Moffett and L5 engineers
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <sys/errno.h>
+#include <os/np.h>
+#include <np/piir.h>
+#include <lib/ptrbox.h>
+#include <string.h>
+
+/*
+ * Allocate a new stack
+ */
+int
+piir_stack_new(struct np_work *work, struct piir_stack **resp)
+{
+    struct piir_stack *stack;
+
+    if (work == NULL || resp == NULL) {
+        return -EINVAL;
+    }
+
+    stack = ptrbox_alloc(sizeof(*stack), work->work_mem);
+    if (stack == NULL) {
+        return -ENOMEM;
+    }
+
+    memset(stack, 0, sizeof(*stack));
+    *resp = stack;
+    return 0;
+}
+
+/*
+ * Push to the PIIR stack
+ */
+int
+piir_push(struct piir_stack *stack, ir_byte_t byte)
+{
+    if (stack == NULL) {
+        return -EINVAL;
+    }
+
+    if (stack->op_i >= PIIR_STACK_SIZE - 1) {
+        return -1;
+    }
+
+    spinlock_acquire(&stack->lock);
+    stack->opstore[stack->op_i++] = byte;
+    spinlock_release(&stack->lock);
+    return 0;
+}
+
+/*
+ * Pop from the PIIR stack
+ */
+int
+piir_pop(struct piir_stack *stack)
+{
+    ir_byte_t byte;
+
+    if (stack == NULL) {
+        return -EINVAL;
+    }
+
+    if (stack->op_i == 0) {
+        return -1;
+    }
+
+    spinlock_acquire(&stack->lock);
+    byte = stack->opstore[--stack->op_i];
+    spinlock_release(&stack->lock);
+    return byte;
+}
