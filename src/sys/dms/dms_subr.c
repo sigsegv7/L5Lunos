458e7c3bf593489440ddbfdd82d2fb108eae3a6f
Ian Moffett <ian@osmora.org>
2025-10-14 19:55:36 -0400

    kern: Add initial DMS framework
    DMS stands for Disk Management and Supervision, drivers can register
    themselves to DMS so that they can be looked up and operated on.
    
    Signed-off-by: Ian Moffett <ian@osmora.org>

---
+++ src/sys/dms/dms_subr.c
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2025 Ian Marco Moffett and L5 engineers
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/types.h>
+#include <sys/errno.h>
+#include <sys/queue.h>
+#include <os/kalloc.h>
+#include <dms/dms.h>
+#include <string.h>
+#include <stdbool.h>
+
+static TAILQ_HEAD(, dms_disk) diskq;
+static bool diskq_is_init = false;
+static disk_id_t next_id = 0;
+
+int
+dms_register(const char *name, struct dms_ops *ops, struct dms_disk **res)
+{
+    struct dms_disk *dp;
+    size_t name_len;
+
+    if (name == NULL || ops == NULL) {
+        return -EINVAL;
+    }
+
+    if (!diskq_is_init) {
+        TAILQ_INIT(&diskq);
+        diskq_is_init = true;
+    }
+
+    dp = kalloc(sizeof(*dp));
+    if (dp == NULL) {
+        return -ENOMEM;
+    }
+
+    name_len = strlen(name);
+    if (name_len >= DISKNAME_MAX - 1) {
+        kfree(dp);
+        return -ENAMETOOLONG;
+    }
+
+    /* Copy the name and set the ID */
+    memcpy(dp->name, name, name_len + 1);
+    dp->id = next_id++;
+    dp->ops = ops;
+    TAILQ_INSERT_TAIL(&diskq, dp, link);
+
+    if (res != NULL) {
+        *res = dp;
+    }
+    return 0;
+}
+
+struct dms_disk *
+dms_get(disk_id_t disk_id)
+{
+    struct dms_disk *dp = NULL;
+    struct dms_disk *dp_cur;
+
+    TAILQ_FOREACH(dp_cur, &diskq, link) {
+        if (dp_cur == NULL) {
+            continue;
+        }
+
+        if (dp_cur->id == disk_id) {
+            dp = dp_cur;
+            break;
+        }
+    }
+
+    return dp_cur;
+}
+
+ssize_t
+dms_write(struct dms_disk *dp, void *p, off_t off, size_t len)
+{
+    struct dms_ops *ops;
+
+    if (dp == NULL || p == NULL) {
+        return -EINVAL;
+    }
+
+    if ((ops = dp->ops) == NULL) {
+        return -EIO;
+    }
+
+    if (ops->write == NULL) {
+        return -ENOTSUP;
+    }
+
+    return ops->write(dp, p, off, len);
+}
+
+ssize_t
+dms_read(struct dms_disk *dp, void *p, off_t off, size_t len)
+{
+    struct dms_ops *ops;
+
+    if (dp == NULL || p == NULL) {
+        return -EINVAL;
+    }
+
+    if ((ops = dp->ops) == NULL) {
+        return -EIO;
+    }
+
+    if (ops->read == NULL) {
+        return -ENOTSUP;
+    }
+
+    return ops->read(dp, p, off, len);
+}
---
+++ src/sys/include/dms/dms.h
@@ -0,0 +1,137 @@
+/*
+ * Copyright (c) 2025 Ian Marco Moffett and L5 engineers
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the project nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _DMS_DMS_H_
+#define _DMS_DMS_H_
+
+#include <sys/queue.h>
+#include <sys/cdefs.h>
+#include <sys/types.h>
+
+/*
+ * Maximum attached disks, from kconf
+ */
+#if defined(__DMS_MAX_DISKS)
+#define DMS_MAX_DISKS __DMS_MAX_DISKS
+#else
+#define DMS_MAX_DISKS 32
+#endif  /* __DMS_MAX_DISKS */
+
+__static_assert(
+    DMS_MAX_DISKS > 0,
+    "maxiumum disks cannot be zro!"
+);
+
+/* Parameters */
+#define DISKNAME_MAX 128    /* Disk name maxlen */
+
+/* ID of the disk */
+typedef uint16_t disk_id_t;
+
+/* Forward declarations */
+struct dms_disk;
+
+/*
+ * Operations that may be performed on a
+ * disk drive
+ *
+ * XXX: `off' and `len' are byte relative
+ */
+struct dms_ops {
+    ssize_t(*write)(struct dms_disk *dp, void *p, off_t off, size_t len);
+    ssize_t(*read)(struct dms_disk *dp, void *p, off_t off, size_t len);
+};
+
+/*
+ * Represents a storage device associated
+ * with the system
+ *
+ * @name: Name of this device
+ * @ops: Operations that can be performed
+ * @data: Driver specific data
+ * @id: ID of the disk
+ * @link: Internal queue link
+ */
+struct dms_disk {
+    char name[DISKNAME_MAX];
+    struct dms_ops *ops;
+    void *data;
+    disk_id_t id;
+    TAILQ_ENTRY(dms_disk) link;
+};
+
+/*
+ * Write to a disk
+ *
+ * @dp: Disk to write to
+ * @p: Input buffer to write
+ * @off: Byte relative offset
+ * @len: Byte relative length
+ *
+ * Returns the length written on success, otherwise a less
+ * than zero errno on failure
+ */
+ssize_t dms_write(struct dms_disk *dp, void *p, off_t off, size_t len);
+
+/*
+ * Read from a disk
+ *
+ * @dp: Disk to read from
+ * @p: Buffer to read into
+ * @off: Byte relative offset
+ * @len: Byte relative length
+ *
+ * Returns the length read on success, otherwise a less
+ * than zero errno on failure
+ */
+ssize_t dms_read(struct dms_disk *dp, void *p, off_t off, size_t len);
+
+/*
+ * Register a device to the DMS core
+ *
+ * @name: Name of device to register
+ * @ops: DMS operations associated with device
+ * @res: Result is written here [NULLable]
+ *
+ * Returns zero on success, otherwise a less than zero
+ * value on failure
+ */
+int dms_register(const char *name, struct dms_ops *ops, struct dms_disk **res);
+
+/*
+ * Retrieve a disk from the DMS core
+ *
+ * @disk_id: ID of disk to retrieve
+ *
+ * Returns the disk descriptor when found, otherwise
+ * NULL on failure or when not found
+ */
+struct dms_disk *dms_get(disk_id_t disk_id);
+
+#endif  /* !_DMS_DMS_H_ */
